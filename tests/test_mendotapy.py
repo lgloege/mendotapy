44254f71ae42951fac4b4cc116e26158fddf176f
Luke Gloege <lukegloege@gmail.com>
2024-04-20 11:47:51 -0400

    test: improve the load() test

--- tests/test_mendotapy.py
+++ tests/test_mendotapy.py
@@ -1,7 +1,8 @@
+import pandas as pd
 import mendotapy
 
 # import pytest
 
 
 def test_load():
-    _ = mendotapy.load()
+    assert isinstance(mendotapy.load(), pd.DataFrame), "load should return a Pandas dataframe"
